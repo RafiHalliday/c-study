c188edbf5b0b3917507d9ad71cb681b9745667a8
RafiHalliday <18223094@std.stei.itb.ac.id>
2026-04-24 10:29:40 +0700

    lesson 25: nested loop

---
+++ 25-nested-loop/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main() {
+
+    // for (int i = 1; i < 4; i++) {   
+    //     for (int j = 1; j < 10; j++) {
+    //         printf("%d ", j);
+    //     }
+        
+    //     printf("\n");
+    // }
+
+    // printf("\n");
+    
+    for (int i = 1; i <= 10; i++) {
+        for (int j = 1; j <= 10; j++) {       
+            printf("%4d ", i * j);
+        }
+        printf("\n");
+    }
+    
+    printf("\n");
+
+    int rows = 0;
+    int columns = 0;
+    char symbol = '\0';
+
+    printf("Enter the # of rows: ");
+    scanf("%d", &rows);
+
+    printf("Enter the # of columns: ");
+    scanf("%d", &columns);
+
+    printf("Enter a symbol to use: ");
+    scanf(" %c", &symbol);
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            printf("%c", symbol);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
