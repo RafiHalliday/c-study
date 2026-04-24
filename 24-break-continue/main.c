ff5cd31bc03638a56cea03306104b66d233ed305
RafiHalliday <18223094@std.stei.itb.ac.id>
2026-04-24 10:20:37 +0700

    lesson 24: break and continue

---
+++ 24-break-continue/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    // break = Break out of a loop (STOP)
+    // continue = Skip current cycle of a loop (SKIP)
+
+    for (int i = 0; i <= 10; i++) {
+        if (i == 4) {
+            continue;
+        }
+
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
